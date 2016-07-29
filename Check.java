3b4a4f22a0bbc040acb7724c14b7dedce1605246
karthika1995 <karthikaus1995@gmail.com>
2016-07-29 15:30:30 +0530

    Add files via upload

---
+++ Check.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Check
+{
+	public static void main(String[] arg)
+	{
+	Scanner in = new Scanner(System.in);
+	int n;
+	System.out.println("Enter n value:");
+	n = in.nextInt();
+	if((n%2) == 0)
+	System.out.println(" The given no. is even");
+	else
+	System.out.println("The given no. is odd");
+	}
+}

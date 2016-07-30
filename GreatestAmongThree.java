bfe900cb9ab489cff31154a7f5e5877ea67f4ab1
karthika1995 <karthikaus1995@gmail.com>
2016-07-30 22:24:26 +0530

    Add files via upload

---
+++ GreatestAmongThree.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class GreatestAmongtThree
+{
+	public static void main(String[] arg)
+	{
+	Scanner s = new Scanner(System.in);
+	int a,b,c;
+	System.out.print("Enter a , b and c value:  ");
+	a = s.nextInt();
+	b = s.nextInt();
+	c = s.nextInt();
+	if(a>b && a>c)
+	System.out.println(" a is greater ");
+	else if(b>c && b>a)
+	System.out.println(" b is greater ");
+	else
+	System.out.println("c is greater");
+	}
+}

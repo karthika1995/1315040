942218079a745e7131453a247d69fcf0e380d165
karthika1995 <karthikaus1995@gmail.com>
2016-07-30 22:35:39 +0530

    Add files via upload

--- GreatestAmongThree.java
+++ GreatestAmongThree.java
@@ -9,16 +9,11 @@ class GreatestAmongtThree
 	a = s.nextInt();
 	b = s.nextInt();
 	c = s.nextInt();
-	if(a>0 && b>0 && c>0)
-	{
 	if(a>b && a>c)
 	System.out.println(" a is greater ");
 	else if(b>c && b>a)
 	System.out.println(" b is greater ");
 	else
 	System.out.println("c is greater");
-	}
-	else
-	System.out.println("Enter positive value");
 	}
 }

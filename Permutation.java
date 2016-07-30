11614144bd2248fea8c0ab0965370c589deb3916
karthika1995 <karthikaus1995@gmail.com>
2016-07-30 19:29:40 +0530

    Update Permutation.java

--- Permutation.java
+++ Permutation.java
@@ -3,7 +3,7 @@
 	{
     public static void main(String[] args) 
 	{
-    Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 	int n,r,x,j=1,k=1,i;
 	System.out.print(" Enter n and r value ");
 	n=input.nextInt();
@@ -19,10 +19,8 @@
 	System.out.println(" (n-r)! =  " + k );
 	x = j / k;
 	System.out.println(" n!/(n-r)!  is   " + x);
-    }
+        }
     else
     System.out.println("enter proper values");
- 
-    }
-    
-}
+       }
+       }

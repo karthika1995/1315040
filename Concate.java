c486dbcba05711b46332f0833c2b78700f9f5836
karthika1995 <karthikaus1995@gmail.com>
2016-08-02 20:46:09 +0530

    Update Concate.java

--- Concate.java
+++ Concate.java
@@ -3,12 +3,21 @@
  {
  public static void main(String[] args) {
         String name1,name2;
-        int x,y;
+        int n;
         Scanner s = new Scanner(System.in);
+        System.out.println("Enter the key");
+        n = s.nextInt();
+        if(n>1)
+        {
         System.out.println("Enter two string ");
         name1 = s.nextLine();
         name2 = s.nextLine();
-        System.out.println("Concated String " +name1 + name2");
+        System.out.println("Concated String " +name1 + name2);
+        }
+        else
+            System.out.println(" The key is >than 1 ");
+        
+   
         
           }
 }		  

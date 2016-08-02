c59cfd04e048ab8287bc1d90ef3719f13d4447d4
karthika1995 <karthikaus1995@gmail.com>
2016-08-02 20:32:54 +0530

    Update Asciivalue.java

--- Asciivalue.java
+++ Asciivalue.java
@@ -3,13 +3,21 @@ import java.util.Scanner;
  {
  public static void main(String[] args)
 	{
-		int	i;
+		int i,n
+		System.out.println(" enter n value ");
+		Scanner in = new Scanner (System.in);
+		n = in.nextInt();
 		System.out.println(" ASCII value for small letters ");
-        for(i='a';i<='z';i++)
+		if(n>0)
+		{
+                for(i='a';i<='z';i++)
 		System.out.println( i );
 		System.out.println(" ASCII value for capital letters ");
 		for(i='A';i<='Z';i++)
 		System.out.println( i );
+		}
+		else
+		System.out.println(" enter positive val ");
 	}
 }
 		

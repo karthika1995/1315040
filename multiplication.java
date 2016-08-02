9027e5c7466cec6cfaabb324a2e9732520ff39ba
karthika1995 <karthikaus1995@gmail.com>
2016-08-02 20:21:15 +0530

    Update multiplication.java

--- multiplication.java
+++ multiplication.java
@@ -7,10 +7,14 @@ class multiplication
 		Scanner in = new Scanner(System.in);
 		System.out.print("Enter any number that table will be displayed");
 		n = in.nextInt();
+		if(n>0
+		{
 		for(i=1;i<=10;i++)
 		{
 			x = n * i;
 			System.out.println( + n + "x" + i + "=" + x); 
 		}
 		}
+		else
+		System.out.print("Enter positive value");
 }

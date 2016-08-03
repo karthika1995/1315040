937578154948428e8286349e7931050ec2036017
karthika1995 <karthikaus1995@gmail.com>
2016-08-03 20:42:18 +0530

    Update Swappingwithvariable.java

--- Swappingwithvariable.java
+++ Swappingwithvariable.java
@@ -3,16 +3,22 @@
  {
  public static void main(String[] args)
 	{
-		int	a,b,c;
+	int	a,b,c;
         Scanner s = new Scanner(System.in);
 		System.out.println("Enter a and b");
         a = s.nextInt();
         b = s.nextInt();
+        if(a>=0 && b>=0)
+        {
 		c = a;
 		a = b; 
 		b = c;
 		System.out.println("After swapping");
 		System.out.println("a = " +a+ "b = " +b+ "c = " +c);
-    }
+        }
+        else
+                System.out.println("Improper value");   
+		
+	}
 }	
 	

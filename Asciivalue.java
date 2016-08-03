f5d314f3bac8eff5c78aabe07e5b48349f68a388
karthika1995 <karthikaus1995@gmail.com>
2016-08-03 20:10:27 +0530

    Add files via upload

--- Asciivalue.java
+++ Asciivalue.java
@@ -3,21 +3,13 @@ import java.util.Scanner;
  {
  public static void main(String[] args)
 	{
-		int i,n
-		System.out.println(" enter n value ");
-		Scanner in = new Scanner (System.in);
-		n = in.nextInt();
+		int	i;
 		System.out.println(" ASCII value for small letters ");
-		if(n>0)
-		{
-                for(i='a';i<='z';i++)
+        for(i='a';i<='z';i++)
 		System.out.println( i );
 		System.out.println(" ASCII value for capital letters ");
 		for(i='A';i<='Z';i++)
 		System.out.println( i );
-		}
-		else
-		System.out.println(" enter positive val ");
 	}
 }
 		

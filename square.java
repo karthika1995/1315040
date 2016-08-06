5277b9afd9b42b03e0831971bac457f649ff0657
karthika1995 <karthikaus1995@gmail.com>
2016-08-06 21:50:02 +0530

    Update square.java

--- square.java
+++ square.java
@@ -1,29 +1,33 @@
     import java.util.Scanner;
 	static sum =0;
 	class square
-    public static void main(String[] args) {
-        
+	{
+	 public static void main(String[] args) {
+        int sum = 0;
         Scanner in = new Scanner(System.in);
         int n,x;
         System.out.print("Enter the number");
         n = in.nextInt();
+        if(n>0)
+        {
         x = n%10;
         while(x>0)
         {
             calc(n%10,x);
             n = n/10;
         }
+        }
+        else
+        System.out.println("error");
         System.out.println("the output is " +sum);
         private static void calc(int y,int z)
         {
-           
-            for(int i=1;i<y;i++)
-			{
-			int p = 1;
-            p = p * i;
-			}
-            sum = sum + p;
-        }
-    }
+            int i,p = 1,sum =0;
+            for( i=1;i<y;i++)
+                p = p * i;
+                 sum = sum + p;
+        }    
+	 	
+	 }
     
 }

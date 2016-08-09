0525fe810f6f83a9b1faa1ad2dff047d1903211e
karthika1995 <karthikaus1995@gmail.com>
2016-08-09 20:59:32 +0530

    Add files via upload

---
+++ Notostring.java
@@ -0,0 +1,16 @@
+
+class Notostring {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       byte []b ={65,66,67,68,69,70};
+       String st = new String(b);
+       System.out.println(st);
+            
+            
+       }
+    
+}
